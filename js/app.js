597eae5722854951929ef9da5a03a5065ebd33bd
LucasCicconi1704 <lucascicconi@gmail.com>
2022-11-14 21:22:42 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import { valida } from "./validaciones.js";
+import { validaText } from "./validaciones.js";
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+    input.addEventListener('blur', (input) => {
+        valida(input.target);
+    });
+
+const textareas = document.querySelectorAll('textarea');
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener('blur', (textarea) => {
+        validaText(textarea.target);
+    })
+});
+
+
+});
